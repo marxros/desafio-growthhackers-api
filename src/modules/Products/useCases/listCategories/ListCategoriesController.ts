bb2d6e06e5a6370e77ea8fd125d392d41aa1d632
Marx Silva <marxmoita@gmail.com>
2022-04-17 16:52:08 -0300

    Feature - listar categorias

---
+++ src/modules/Products/useCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+class ListCategoriesController {
+
+  async handle(request: Request, response: Response): Promise<Response> {
+
+    const listCategoryUseCase = container.resolve(ListCategoriesUseCase);
+
+    const categories = await listCategoryUseCase.execute();
+
+    return response.json(categories);
+  }
+}
+
+export { ListCategoriesController };
--- src/routes/categories.router.ts
+++ src/routes/categories.router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { CreateCategoryController } from '../modules/Products/useCases/createCategory/CreateCategoryController';
+import { ListCategoriesController } from '../modules/Products/useCases/listCategories/ListCategoriesController';
 
 const categoriesRoutes = Router();
 
 const createCategoryController = new CreateCategoryController();
+const listCategoriesController = new ListCategoriesController();
 
 categoriesRoutes.post('/', createCategoryController.handle);
-
+categoriesRoutes.get('/', listCategoriesController.handle);
 
 export { categoriesRoutes };
