039e5e9d9fd0cb64c274ddb4d8ea957dcb0838dc
Marx Silva <marxmoita@gmail.com>
2022-04-21 00:47:38 -0300

    adicionando cors

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,15 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from '../swagger.json';
 
+import cors from 'cors';
+
 import './database';
 import './shared/container';
 
 import { router } from './routes';
 
 const app = express();
-
+app.use(cors({}));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 
